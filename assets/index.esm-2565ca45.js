ef80028e6d666e7d06bc16ae073a4e7416c4656e
TuralQasim <TuralQasim@users.noreply.github.com>
2023-09-07 19:30:43 +0000

    deploy: 9d636d8872f3d5005814c5d645e9392cb88bd9a6

---
+++ assets/index.esm-2565ca45.js
@@ -0,0 +1,1 @@
+import{G as o}from"./index-4de948f9.js";function n(t){return o({tag:"svg",attr:{viewBox:"0 0 24 24",fill:"none",stroke:"currentColor",strokeWidth:"2",strokeLinecap:"round",strokeLinejoin:"round"},child:[{tag:"line",attr:{x1:"7",y1:"17",x2:"17",y2:"7"}},{tag:"polyline",attr:{points:"7 7 17 7 17 17"}}]})(t)}export{n as F};
